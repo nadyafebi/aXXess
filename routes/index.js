f3d2fa1896167a7960680c6f955c7539387843ee
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-07-08 16:16:45 -0700

    Add driver page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/driver', function(req, res, next) {
+  res.render('driver', { title: 'Driver' });
+});
+
 module.exports = router;
