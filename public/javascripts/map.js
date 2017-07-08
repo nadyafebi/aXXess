f60a56dd87b098b957ab5e889542c680da710e0e
Diane27 <dianetobit@gmail.com>
2017-07-08 16:35:02 -0700

    add directions on the map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,35 @@
 var map;
+var directionsDisplay;
+var directionsService;
+var haight;
+var oceanBeach;
+
+
 function initMap() {
+	directionsService = new google.maps.DirectionsService();
+
+haight = new google.maps.LatLng(37.7699298, -122.4469157);
+oceanBeach = new google.maps.LatLng(37.7683909618184, -122.51089453697205);
+	directionsDisplay = new google.maps.DirectionsRenderer();
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
+    
+
+  });
+  directionsDisplay.setMap(map);
+
+}
+
+function calcRoute() {
+  var request = {
+    origin: haight,
+    destination: oceanBeach,
+    travelMode: 'DRIVING'
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
   });
 }
