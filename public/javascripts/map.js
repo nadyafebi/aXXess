74224fac9d9d51ec3017ec2c148e3524239a6191
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-07-08 21:51:45 -0700

    Add text system and change icon

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -7,10 +7,13 @@ var directionsService;
 var start;
 var end;
 
+// ===
+// MAP
+// ===
 function initMap() {
   // Connect to Google Services.
   directionsService = new google.maps.DirectionsService();
-  directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
 
   // Default position.
   start = new google.maps.LatLng(37.7699298, -122.4469157);
@@ -30,12 +33,50 @@ function initMap() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
       start = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      findShelter();
     });
+  }
+
+  function findShelter() {
+    // Get shelter address.
+    var address = document.getElementById("address").innerHTML;
+    var url = "https://crossorigin.me/https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBbD4TnXobioui4xPnHAttFuhhagzDoijU";
+    $.getJSON(url, function(data) {
+      console.log(data)
+      end = data.results[0].geometry.location;
+      userDone();
+    });
+  }
+
+  // Get direction.
+  function userDone() {
+    console.log(end);
+    calcRoute();
   }
 }
 
+
+// =========
+// DIRECTION
+// =========
+
 // Calculate route from start to end.
 function calcRoute() {
+  // Draw markers.
+  var truckImage = "img/truck.png";
+  var truckMarker = new google.maps.Marker({
+    position: start,
+    map: map,
+    icon: truckImage
+  });
+
+  var womanImage = "img/tampon.png";
+  var womanMarker = new google.maps.Marker({
+    position: end,
+    map: map,
+    icon: womanImage
+  });
+
   var request = {
     origin: start,
     destination: end,
@@ -46,4 +87,12 @@ function calcRoute() {
       directionsDisplay.setDirections(result);
     }
   });
+}
+
+// =======
+// MARKERS
+// =======
+
+function getMarker() {
+
 }
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,15 @@ var accountSid = 'AC7e4969825a94132e9d2684bc11ae547b';
 var authToken = '7a7876cd6794a7142628ec81238f04ef';
 var client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+var address = "321 Golf Club Road, Pleasant Hill, CA";
+var replaced;
+format();
+
+// Format address.
+function format() {
+  var str = address;
+  replaced = str.split(' ').join('+');
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,15 +24,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/driver', function(req, res, next) {
-  res.render('driver', { title: 'Driver' });
+  res.render('driver', { title: 'Driver', location: replaced });
+});
+
+router.get('/request', function(req, res, next) {
+  res.render('request', { title: 'Request' });
 });
 
 // POST
 
 router.post('/sms', (req, res) => {
+  address = req.body.Body;
+  format();
   const twiml = new MessagingResponse();
 
-  twiml.message('The Robots are coming! Head for the hills!');
+  twiml.message('You said' + req.body.Body);
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
@@ -34,9 +49,9 @@ module.exports = router;
 // Twilio Test
 /*
 client.messages.create({
-   to: "+19257255730",
+   to: "+19253395106",
    from: "+15103301417",
-   body: "This is from hack4humanity.",
+   body: "HELLO DIANE"
 }, function(err, message) {
    console.log(message.sid);
 });
