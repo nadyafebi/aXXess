1b57aefd6c5e4af30a388c4ace8056d7961c7e6e
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-07-08 19:38:53 -0700

    Changes
    Add Twilio, add documentation to map.js

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,32 +1,40 @@
+// Google Maps Variable
 var map;
 var directionsDisplay;
 var directionsService;
+
+// My Variable
 var start;
 var end;
 
-
 function initMap() {
+  // Connect to Google Services.
   directionsService = new google.maps.DirectionsService();
   directionsDisplay = new google.maps.DirectionsRenderer();
 
+  // Default position.
   start = new google.maps.LatLng(37.7699298, -122.4469157);
   end = new google.maps.LatLng(37.7683909618184, -122.51089453697205);
 
+  // Create the map.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.8272,
-             lng: -122.2913},
+    // Center at SF Bay Area.
+    center: {lat: 37.8272, lng: -122.2913},
     zoom: 10
   });
+
+  // Set Direction Service to map.
   directionsDisplay.setMap(map);
 
+  // Get user location.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
       start = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      console.log(start);
     });
   }
 }
 
+// Calculate route from start to end.
 function calcRoute() {
   var request = {
     origin: start,
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
+// Express Setup
+var http = require('http');
 var express = require('express');
 var router = express.Router();
+
+// Twilio Setup
+var accountSid = 'AC7e4969825a94132e9d2684bc11ae547b';
+var authToken = '7a7876cd6794a7142628ec81238f04ef';
+var client = require('twilio')(accountSid, authToken);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,4 +18,26 @@ router.get('/driver', function(req, res, next) {
   res.render('driver', { title: 'Driver' });
 });
 
+// POST
+
+router.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
 module.exports = router;
+
+// Twilio Test
+/*
+client.messages.create({
+   to: "+19257255730",
+   from: "+15103301417",
+   body: "This is from hack4humanity.",
+}, function(err, message) {
+   console.log(message.sid);
+});
+*/
